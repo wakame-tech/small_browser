65bce981893fa9bfb6a940ad2bf6a149ea119019
wakame-tech <Kamata1919@gmail.com>
2024-06-23 03:23:32 +0900

    chore: deploy to Vercel (#2)

---
+++ vercel-build.sh
@@ -0,0 +1,6 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+cd ./renderer && wasm-pack build . --target web --out-dir ../web/src/wasm && cd ..
+cd ./web/ && npm install && npm run build
